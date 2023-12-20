be32373bc4f542c150d7602b9d1c721ee5adff5d
awargnier <60512377+awargnier@users.noreply.github.com>
2023-12-20 17:07:56 +0100

    parametrization

---
+++ tests/test_bulk_parametrization.py
@@ -0,0 +1,10 @@
+import pytest
+from tests.test_tips import total_with_tip
+@pytest.mark.parametrize("num1,num2,expectation", [
+  (10,20,12),
+  (100,20,120),
+  (0,0,5)
+])
+
+def test_tip_bulk(num1,num2, expectation):
+  assert total_with_tip(num1,num2) == expectation
